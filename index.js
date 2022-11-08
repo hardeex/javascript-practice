ca78fce627dfb100d9ad8bf06cf5e7aad928f660
Jamiu Adewale Yusuf <jamiuadewaleyusuf@gmail.com>
2022-11-08 17:15:44 +0100

    changing the source of a file to turn On and OFF a bulb

---
+++ index.js
@@ -0,0 +1,19 @@
+//alert('Buy databundle at a discounte rate of 5% and 15% for registered members');
+console.log(document);
+
+document.getElementById("ptxt").innerHTML = "changed the text using DOM";
+document.getElementById('ptxt').style.fontSize = '2rem';
+document.getElementById('ptxt').style.color = 'red';
+
+document.getElementById('bulb-on').onclick =src
+onclick.getElementById('bulb-on')
+
+if (document.getElementById('bulb-on')) {
+    
+    
+} else {
+    
+}
+
+
+
